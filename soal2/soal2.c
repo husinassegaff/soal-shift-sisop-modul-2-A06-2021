67eceb743605473797ee1402cee1ff9ea70535aa
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2021-04-25 21:41:42 +0700

    fixed no 2

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -6,13 +6,9 @@
 #include <wait.h>
 #include <dirent.h>
 
-<<<<<<< HEAD
-char ziplocation[] = "/home/husin/modul2/pets/";
-=======
 char ziplocation[] = "/home/husin/modul2/petshop";
 const char semicolon[] = ";";
 const char udr[] = "_";
->>>>>>> Rafki
 
 void ForkWaitFunction(char bash[], char *arg[]){
     int status;
@@ -26,81 +22,6 @@ void ForkWaitFunction(char bash[], char *arg[]){
     }
 }
 
-<<<<<<< HEAD
-
-void ExecuteFilesRecursively(char *basePath)
-{
-    char path[1000];
-    const char *semicolon = ";", *underscore = "_";
-    struct dirent *dp;
-    DIR *dir = opendir(basePath);
-
-    if (!dir){
-        return;
-    }
-
-    while ((dp = readdir(dir)) != NULL)
-    {
-        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
-        {
-            if(dp->d_type == DT_DIR){
-                char petslocation[100] = "/home/husin/modul2/pets/";
-                strcat(petslocation, dp->d_name);
-
-                char *argvrmFolder[] = {"rm", "-rf", petslocation, NULL};
-                ForkWaitFunction("/usr/bin/rm", argvrmFolder);
-            }
-            else{
-                char typepetsfoldername[100] = "";
-                char typepetslocation[100] = "/home/husin/modul2/petshop/";
-                
-                strcpy(typepetsfoldername, dp->d_name);
-                strtok(typepetsfoldername, semicolon);
-                
-                strcat(typepetslocation, typepetsfoldername);
-
-                char *argvmkdirTypePets[] = {"mkdir", "-p", typepetslocation, NULL};
-                ForkWaitFunction("/bin/mkdir", argvmkdirTypePets);
-
-                char movefile[100] = "/home/husin/modul2/pets/";
-                strcat(movefile, dp->d_name);
-
-                if(!(strstr(movefile, underscore))){
-                    char *argvmvFile[] = {"mv", "-f", movefile, typepetslocation, NULL};
-                    ForkWaitFunction("/usr/bin/mv", argvmvFile);
-                }
-                else{
-                    char copyfile[100] = "/home/husin/modul2/pets/";
-                    strcat(copyfile, dp->d_name);
-                    strtok(copyfile, ".");
-                    strcat(copyfile, "(1)");
-                    strcat(copyfile, ".jpg");
-
-                    char *argvcpTypePets[] = {"cp", movefile, copyfile, NULL};
-                    ForkWaitFunction("/usr/bin/cp", argvcpTypePets);
-
-                    if(!(strstr(movefile, "(1)"))){
-                        char *argvmvFile2[] = {"mv", "-f", movefile, typepetslocation, NULL};
-                        ForkWaitFunction("/usr/bin/mv", argvmvFile2);
-                    }
-                }
-    
-            }
-  
-            // Construct new path from our base path
-            strcpy(path, basePath);
-            strcat(path, "/");
-            strcat(path, dp->d_name);
-
-            ExecuteFilesRecursively(path);
-        }
-    }
-
-    closedir(dir);
-}
-
-
-=======
 void listDir(char *basePath)
 {
 
@@ -293,26 +214,11 @@ void listDir(char *basePath)
     closedir(dir);
 }
             
->>>>>>> Rafki
 
 int main() {
     int status;
     pid_t child1;
 
-<<<<<<< HEAD
-    char *argvmkdirPetshop[] = {"mkdir", "-p", "petshop", NULL};
-    char *argvmkdirPets[] = {"mkdir", "-p", "pets", NULL};
-    char *argvunzipPets[] = {"unzip", "pets.zip", "-d", "pets", NULL};
-
-    ForkWaitFunction("/bin/mkdir", argvmkdirPetshop);
-    ForkWaitFunction("/bin/mkdir", argvmkdirPets);
-    ForkWaitFunction("/usr/bin/unzip", argvunzipPets);
-    
-    ExecuteFilesRecursively(ziplocation);
-
-    return 0;
-}
-=======
     char *argvmkdirpets[] = {"mkdir", "petshop", NULL};
     char *argvunzippets[] = {"unzip", "pets.zip", "-d", "petshop", NULL};
     
@@ -323,4 +229,3 @@ int main() {
 
     return 0;
 }
->>>>>>> Rafki
