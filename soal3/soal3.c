d1504e7ed47d64beb0b4bb90a703adaf2fa629d8
Richard Asmarakandi <richardasmarakandi@gmail.com>
2021-04-25 22:42:10 +0700

    Uploaded soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -40,10 +40,94 @@ void downloadgambar(char dir[])
     execv("/usr/bin/wget", argv);
 }
 
-int main ()
+void chip(char *textdl, int adder)
+{
+	for (int a=0;textdl[a]!='\0';a++)
+	{
+		if (textdl[a]>=65 && textdl[a]<=90)
+		{
+		adder = adder%26;
+		textdl[a] += adder;
+			if (textdl[a] > 90)
+			{
+			textdl[a] -= 26;
+			}
+		}
+		else if (textdl[a]>=97 && textdl[a]<=122)
+		{
+		adder = adder%26;
+			if (textdl[a]+adder > 122)
+			{
+			textdl[a] -= 26;
+			}
+			textdl[a] += adder;
+		}
+		if (textdl[a]>=48 && textdl[a]<=57)
+		{
+			adder = adder%10;
+			if (textdl[a]+adder > 57)
+			{
+			textdl[a] -= 10;
+			}
+			textdl[a] += adder;
+		}
+	}
+}
+
+void cetakdownloadsuccess(char namafolder[])
+{
+    char downloadstatus[150];
+    char textdl[20] = "Download Success";
+    sprintf(downloadstatus, "%s/status.txt",namafolder);
+    chip(textdl,5);
+    FILE *downloaded = fopen(downloadstatus,"w");
+    fprintf(downloaded,"%s",textdl);
+    fclose(downloaded);
+}
+
+void bikinkiller(int argc,char **argv, int pid)
+{
+    FILE *fkiller = fopen("killer.sh","w");
+
+    if (argc < 2)
+    {
+        printf("Kasih parameter dong yank :(\n");
+        exit(0);
+    }
+    if (strcmp(argv[1],"-z") == 0)
+    {
+        fprintf(fkiller, "#!/bin/bash\nkillall -9 soal3\nrm \"$0\"");
+    }
+    else if (strcmp(argv[1],"-x") == 0)
+    {
+        fprintf(fkiller, "#!/bin/bash\nkill %d\nrm \"$0\"", pid);
+    }
+
+    fclose(fkiller);
+
+    if(fork() == 0)
+    {
+        char *chmodarg[] = {"chmod", "+x", "killer.sh", NULL};
+        execv("/bin/chmod", chmodarg);
+    }
+}
+
+void makezip(char namafolder[])
+{
+    char zipname[150];
+    sprintf(zipname,"%s.zip",namafolder);
+    char *argv[] = {"zip", "-rmq", zipname, namafolder,NULL};
+    if (fork() == 0)
+        execv("/usr/bin/zip",argv);
+    printf("Process Completed.\n");
+}
+
+int main (int argc, char **argv)
 {
     int firstrun=1;
-    pid_t child_id;
+    pid_t pid, child_id;
+    bikinkiller(argc,argv,(int)getpid());
+    printf("Killer Created.\n");
     while(1)
     {
         setwaktusekarang();
@@ -52,8 +136,8 @@ int main ()
         {
             buatfolder(buffer);
         }
-        child_id = fork();
         while (opendir(buffer) == NULL);
+        printf("Folder Created.\n");
         char dir[80];
         strcpy(dir,buffer);
         child_id = fork();
@@ -61,16 +145,19 @@ int main ()
         {
             for (int a=0;a<10;a++)
             {
-                printf("Download Success\n");
                 getlink();
                 child_id = fork();
                 setwaktusekarang();
-                char finaldir[100];
+                char finaldir[200];
                 sprintf(finaldir,"%s/%s",dir,buffer);
                 if (child_id==0)
                     downloadgambar(finaldir);
+                printf("Download Success.\n");
                 sleep(5);
             }
+            cetakdownloadsuccess(dir);
+            sleep(1);
+            makezip(dir);
         }
         sleep(40);
     }
